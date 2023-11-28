5abdfeff9f205f49a48f46e592789f444ea81ca0
Seongmin Kim <atez.dev@gmail.com>
2023-11-29 00:47:04 +0900

    Change: sample api-server source code structure

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from flask import Flask
-from flask_restx import Api
+from flask_restx import Api, Resource
 
 app = Flask(__name__)
 api = Api(app)  
+
+@api.route('/getmealAmount') 
+class MealAmount(Resource):
+    def get(self): 
+        return {
+            "amount": 100
+        }
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=3000)
